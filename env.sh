de2a66d4ad00dd66a38b09d75148969f0c6672a3
bn3monkey <bn3monkey@naver.com>
2021-07-01 17:05:18 +0900

    add notion api to project

---
+++ env.sh
@@ -0,0 +1,8 @@
+# 1. Install Node.js in https://nodejs.org/en/download/
+#    https://nodejs.org/en/download/에서 Node.js를 다운로드하세요
+
+# 2. npm install yarn
+
+# 3. yarn init
+
+4. yarn add @notionhq/client
