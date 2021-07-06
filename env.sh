7da62289a3732e8c72c77972accff84c7d2e9be1
bn3monkey <bn3monkey@naver.com>
2021-07-06 19:52:34 +0900

    add setting.json

--- env.sh
+++ env.sh
@@ -1,16 +1,16 @@
 # 1. Install Node.js in https://nodejs.org/en/download/
 #    https://nodejs.org/en/download/에서 Node.js를 다운로드하세요
 
+# 5. install typescript
+#npm install -g typescript
+#npm upgrade typescript
+
 # 2. npm install yarn
 
 # 3. yarn init
 
 # 4. install express
 yarn add express
-
-# 5. install typescript
-npm install -g typescript
-npm upgrade typescript
 
 # 5. install notion client 
 yarn add @notionhq/client
